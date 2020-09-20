9609ccfdbe686a93b48bf8f73bc37f4b26490fa2
ykurata <yasukokurata@gmail.com>
2020-09-20 00:14:27 -0400

    set up payment route

--- server/index.js
+++ server/index.js
@@ -14,5 +14,38 @@ app.get("/", (req, res) => {
   res.send("It workes");
 });
 
+app.post("/payment", (req, res) => {
+  const { product, token } = req.body;
+  console.log("product", product);
+  console.log("price", product.price);
+  const idempontencyKey = uuid();
+
+  return stripe.customers
+    .create({
+      email: token.email,
+      source: token.id,
+    })
+    .then((customer) => {
+      stripe.charges.create(
+        {
+          amount: product.price * 100,
+          currency: "usd",
+          customer: customer.id,
+          receipt_email: token.email,
+          description: `Purchase of ${product.name}`,
+          shipping: {
+            name: token.card.name,
+            address: {
+              country: token.card.address_country,
+            },
+          },
+        },
+        { idempontencyKey }
+      );
+    })
+    .then((result) => res.status(200).json(result))
+    .catch((err) => console.log(err));
+});
+
 // listen
 app.listen(8282, () => console.log("Listening at port 8282"));
